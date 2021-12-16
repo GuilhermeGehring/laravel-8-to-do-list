3b8af7d1587cbae541c2413eada56ae876f112af
Guilherme <guilherme.gehring@gmail.com>
2021-12-16 15:23:20 -0300

    Adição de testes da api

--- app/Http/Resources/TodoResource.php
+++ app/Http/Resources/TodoResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class TodoResource extends JsonResource
 {
@@ -14,6 +15,11 @@ class TodoResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'title' => strtolower($this->name),
+            'body' => $this->description,
+            'completed' => $this->completed ? 'S' : 'N',
+            'date' => Carbon::create($this->created_at)->format('Y-m-d'),
+        ];
     }
 }
